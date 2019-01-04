b7f16739221c963802f8e88c7ff96948b6ba3ccf
David B.R <davidbojart@gmail.com>
2019-01-04 08:17:02 +0100

    Update sistema_riego.ino
    Incluido modulo bluetooth.

--- sistema_riego.ino
+++ sistema_riego.ino
@@ -1,26 +1,38 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
 
-int MotorDer1=7;
-int MotorDer2=8;
-int PWM_Derecho=10;
-int contador = 0;
-
+SoftwareSerial bt(10, 11); // RX | TX
 LiquidCrystal_I2C lcd (0x27, 16, 2);
+
+// Pines para el motor
+int IN3=7;
+int IN4=6;
+int ENB=5;
+
+int contador = 0;
 
 const int sensorPin = A0; // Salida analogica del sensor de humedad
 
 void setup(){
 
-  Serial.begin(9600); 
+  Serial.begin(9600);
+  
+  // Para la pantalla
   Wire.begin();
   lcd.begin(16,2); // Definimos que es un LCD de 16x2
   lcd.backlight(); // Enciende la luz
   lcd.clear(); // limpiamos la pantalla
 
-  pinMode(MotorDer1, OUTPUT);
-  pinMode(MotorDer2, OUTPUT);
-  pinMode(PWM_Derecho, OUTPUT);
+  // Para el motor
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  // Para el bluetooth
+  bt.begin(9600); // activamos el serial del bluetooth
+  pinMode(9, OUTPUT); // activamos el modulo bluetooth mediante el pin 9 del arduino
+  digitalWrite(9,HIGH);
 
 }
 
@@ -33,33 +45,35 @@ void pantalla(int humedad) {
   lcd.setCursor(8, 0);
   lcd.print("S: ");
   
-  if(humedad > 500){
+  if(humedad > 600){
     lcd.print("NOK");
   }
   else{
     lcd.print("OK");
   }
   lcd.setCursor(0, 1); // Ponemos el cursor en la segunda Fila
-  lcd.print("Contador: ");
+  lcd.print("Regado: ");
   lcd.print(contador);
   
 }
 
 void apagado_motor(){ 
-/* Funcion para que el motor pare */
-  digitalWrite(MotorDer1,LOW);
-  digitalWrite(MotorDer2,LOW);
   
-  analogWrite(PWM_Derecho,150); 
+// Funcion para que el motor pare
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,LOW);
+  
+  analogWrite(ENB,150); 
 
 }
 
 void encencido_motor(){ 
-/* Funcion para que el motor se encienda */
-  digitalWrite(MotorDer1,LOW);
-  digitalWrite(MotorDer2,HIGH);
   
-  analogWrite(PWM_Derecho,150);
+// Funcion para que el motor se encienda
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+  
+  analogWrite(ENB,150);
  
   lcd.setCursor(0, 0);
   lcd.print("               ");
@@ -68,7 +82,7 @@ void encencido_motor(){
 
   lcd.setCursor(0,1);
   
-  delay(6000); // Regamos durante 6 segundos
+  delay(2000); // Regamos durante 2 segundos
   
   lcd.clear();
   
@@ -81,23 +95,26 @@ void encencido_motor(){
 
 void humedad_suelo(int humedad) {
   
-  int humedad = analogRead(sensorPin);
-
-  if(humedad > 500){ // salida analogica
+  if(humedad > 600){ // salida analogica
    
     encencido_motor();
 
   }
   
   pantalla(humedad);
-  delay(10000); // Esperamos 10 segundos antes de volver a medir la humedad
+  
+  delay(30000); // Esperamos 30 segundos antes de volver a medir la humedad
   
 }
 
-
 void loop()
 {
-//  int humedad = analogRead(sensorPin);
+  int humedad = analogRead(sensorPin);
   humedad_suelo(humedad);
+  
+  Serial.println(humedad);
+  bt.println(String(humedad));
+  
+  delay(2000);
 
 }
